5aded9e3485576789fb415a203e771d293af87d9
shriwise <pshriwise@gmail.com>
2017-03-03 17:13:30 -0600

    Will respond to cases without space between value and units now. Adding more conversion functions for some reason. Removing compiled python file.

--- unitbot_plugins.py
+++ unitbot_plugins.py
@@ -9,21 +9,27 @@ def F_to_C(temp_F):
 def C_to_F(temp_C):
     return temp_C*(9.0/5.0) + 32.0
 
-@respond_to('(.*) F', re.IGNORECASE)
+def C_to_K(temp_C):
+    return temp_C+273.15
+
+def F_to_K(temp_F):
+    return C_to_K(F_to_C(temp_F))
+
+@respond_to('(.*)F', re.IGNORECASE)
 def temptoC(message, incoming_message):
     print "Responding to", incoming_message
     try:
-        temp_f = float(incoming_message.split()[-1])
+        temp_f = float(incoming_message.split()[-1].lower().replace("f",""))
     except:
         return
     temp_c = F_to_C(temp_f)
     message.reply("That is {:.2f} C!".format(temp_c))
 
-@respond_to('(.*) C', re.IGNORECASE)
+@respond_to('(.*)C', re.IGNORECASE)
 def temptoF(message, incoming_message):
     print "Responding to", incoming_message
     try:
-        temp_c = float(incoming_message.split()[-1])
+        temp_f = float(incoming_message.split()[-1].lower().replace("c",""))
     except:
         return
     temp_f = C_to_F(temp_c)
