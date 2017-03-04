358af89e66db0e5d7938f6690981f00420b41a47
Mouginot B <mouginot.baptiste@gmail.com>
2017-03-03 19:22:48 -0600

    noew use quantities lib + change slightly the out msg

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,8 @@
+import private
+
 DEFAULT_REPLY= "Culdn't here right. Did u say brains?"
 PLUGINS = [ 'slackbot.plugins',
             'unitbot_plugins']
-ERRORS_TO = 'shriwise'
-
+ERRORS_TO = private.ERRORS_TO
+API_TOKEN= private.API_TOKEN
 
--- unitbot_plugins.py
+++ unitbot_plugins.py
@@ -3,6 +3,10 @@ from slackbot.bot import listen_to
 import quantities as q
 import re
 
+
+degC_relativ_cte = 32.0*5.0/9.0
+
+
 def F_to_C(temp_F):
     return (temp_F - 32.0)*(5.0/9.0)
 
@@ -17,20 +21,23 @@ def F_to_K(temp_F):
 
 @respond_to('(.*)F', re.IGNORECASE)
 def temptoC(message, incoming_message):
-    print "Responding to", incoming_message
+    print("Responding to", incoming_message)
     try:
-        temp_f = float(incoming_message.split()[-1].lower().replace("f",""))
+        temp_in = float(incoming_message.split()[-1].lower().replace("f",""))
     except:
         return
-    temp_c = F_to_C(temp_f)
-    message.reply("That is {:.2f} C!".format(temp_c))
+    temp = temp_in * q.degF
+    temp.units = q.degC
+    temp -= degC_relativ_cte * q.degC
+    message.reply("{} °F corresponds to {:.2f} °C!".format(incoming_message, float(temp.magnitude)))
 
 @respond_to('(.*)C', re.IGNORECASE)
 def temptoF(message, incoming_message):
-    print "Responding to", incoming_message
+    print("Responding to", incoming_message)
     try:
-        temp_c = float(incoming_message.split()[-1].lower().replace("c",""))
+        temp_in = float(incoming_message.split()[-1].lower().replace("c",""))
     except:
         return
-    temp_f = C_to_F(temp_c)
-    message.reply("That is {:.2f} F!".format(temp_f))
+    temp = temp_in * q.degC + degC_relativ_cte * q.degC
+    temp.units = q.degF
+    message.reply("{} °C corresponds to {:.2f} °F!".format(incoming_message, float(temp.magnitude)))
