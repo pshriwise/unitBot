f59610d7a962f5998e8c2a463ea2de8f09ef9236
shriwise <pshriwise@gmail.com>
2017-03-05 01:51:50 -0600

    Filtering out channels and tailoring reply type based on channel vs. direct message.

--- unitbot_plugins.py
+++ unitbot_plugins.py
@@ -3,6 +3,7 @@ from slackbot.bot import listen_to
 import quantities as q
 import re
 
+approved_channels = ['unit_test']
 
 F_to_C_fix = -32.0*5.0/9.0
 C_to_F_fix = +32.0*5.0/9.0
@@ -16,6 +17,21 @@ def convert(val, to):
         val += C_to_F_fix * q.degC
     return val
 
+def is_channel(message):
+    return ('is_channel' in message.channel._body.keys() and
+            message.channel._body['is_channel'] == True)
+
+def is_approved(message):
+    return message.channel._body['name'] in approved_channels
+
+def dispatch(message, output):
+    #see if this came from a channel (listen_to)
+    if is_channel(message):
+        #make sure the channel is in approved list for posting
+        if is_approved(message): message.send(output)
+    else:
+        message.reply(output)
+
 " Temperature conversion "
 
 F_match = '(.*)F'
@@ -27,7 +43,8 @@ def FtoC(message,incoming_message):
     except:
         return
     val = convert(val_in * q.degF, to = q.degC)
-    message.reply("{}F corresponds to {:.2f}C!".format(val_in, float(val.magnitude)))
+    out_str = "{}F corresponds to {:.2f}C!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
 
 C_match = '(.*)C'
 @respond_to(C_match, re.IGNORECASE)
@@ -39,7 +56,8 @@ def CtoF(message, incoming_message):
     except:
         return
     val = convert(val_in * q.degF, to = q.degC) 
-    message.reply("{}C corresponds to {:.2f}F!".format(val_in, float(val.magnitude)))
+    out_str = "{}C corresponds to {:.2f}F!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
 
 " Distance "
 m_match = '(.*)m'
@@ -52,7 +70,8 @@ def MtoInch(message, incoming_message):
     except:
         return
     val = convert(val_in * q.m, to = q.inch) 
-    message.reply("{} m corresponds to {:.2f} \"!".format(val_in, float(val.magnitude)))
+    out_str = "{} m corresponds to {:.2f} \"!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
 
 cm_match = '(.*)cm'
 @respond_to(cm_match, re.IGNORECASE)
@@ -64,7 +83,8 @@ def InchtoM(message, incoming_message):
     except:
         return
     val = convert(val_in * q.cm, to = q.inch) 
-    message.reply("{} m corresponds to {:.2f} \"!".format(val_in, float(val.magnitude)))
+    out_str = "{} m corresponds to {:.2f} \"!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
 
 inch_match = '(.*)"'
 inch_match1 = '(.*)in'
@@ -79,7 +99,8 @@ def CMtoInch(message, incoming_message):
     except:
         return
     val = convert(val_in * q.inch, to = q.m) 
-    message.reply("{} \" corresponds to {:.2f} m!".format(val_in, float(val.magnitude)))
+    out_str = "{} \" corresponds to {:.2f} m!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
 
 mile_match = '(.*)mile'
 @respond_to(mile_match, re.IGNORECASE)
@@ -91,7 +112,8 @@ def MiletoKM(message, incoming_message):
     except:
         return
     val = convert(val_in * q.mile, to = q.km) 
-    message.reply("{} mile corresponds to {:.2f} km!".format(val_in, float(val.magnitude)))
+    out_str = "{} mile corresponds to {:.2f} km!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
 
 km_match = '(.*)km'
 @respond_to(km_match, re.IGNORECASE)
@@ -103,4 +125,5 @@ def KMtoMile(message, incoming_message):
     except:
         return
     val = convert(val_in * q.km, to = q.mile) 
-    message.reply("{} km corresponds to {:.2f} mile!".format(val_in, float(val.magnitude)))
+    out_str = "{} km corresponds to {:.2f} mile!".format(val_in, float(val.magnitude))
+    dispatch(message, out_str)
