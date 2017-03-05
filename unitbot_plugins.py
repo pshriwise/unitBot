c726bc11b8616ad72e6261147c266613008c9971
shriwise <pshriwise@gmail.com>
2017-03-05 11:22:05 -0600

    Using quantities to format output now. Refactor of responding functions.

--- unitbot_plugins.py
+++ unitbot_plugins.py
@@ -1,20 +1,24 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 import quantities as q
+from quantities import markup
 import re
+
+#allows nice output from quantities
+markup.config.use_unicode = True
 
 approved_channels = ['unit_test']
 
 F_to_C_fix = -32.0*5.0/9.0
-C_to_F_fix = +32.0*5.0/9.0
+C_to_F_fix = +32.0
 
 def convert(val, to):
-    input_units = val.units
-    if(input_units == q.degC and to == q.degF):
-        val += F_to_C_fix * q.degF
+    input_units = val.units.copy()
     val.units = to
+    if(input_units == q.degC and to == q.degF):
+        val += C_to_F_fix * q.degF
     if(input_units == q.degF and to == q.degC):
-        val += C_to_F_fix * q.degC
+        val += F_to_C_fix * q.degC
     return val
 
 def is_channel(message):
@@ -32,6 +36,19 @@ def dispatch(message, output):
     else:
         message.reply(output)
 
+def generate_ouput(value, input_units, output_units):
+    quantity_in = value * input_units
+    quantity_out = convert(quantity_in.copy(), to = output_units)
+    quantity_out = round(quantity_out,2) * output_units
+    output = "{:} corresponds to {:}!".format(quantity_in, quantity_out)
+    return output
+
+def parse_value(message_content, replace_string):
+    replace_string = replace_string.lower()
+    val_in = message_content.split()[-1].lower()
+    val_in = val_in.replace(replace_string,"")
+    return float(val_in)
+    
 " Temperature conversion "
 
 F_match = '(.*)F'
@@ -39,24 +56,21 @@ F_match = '(.*)F'
 @listen_to(F_match, re.IGNORECASE)
 def FtoC(message,incoming_message):
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("f",""))
+        val_in = parse_value(incoming_message, "F")
+        out_str = generate_ouput(val_in, q.degF, q.degC)
     except:
         return
-    val = convert(val_in * q.degF, to = q.degC)
-    out_str = "{}F corresponds to {:.2f}C!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
 
 C_match = '(.*)C'
 @respond_to(C_match, re.IGNORECASE)
 @listen_to(C_match, re.IGNORECASE)
 def CtoF(message, incoming_message):
-    print("Responding to", incoming_message)
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("c",""))
+        val_in = parse_value(incoming_message, "C")
+        out_str = generate_ouput(val_in, q.degC, q.degF)
     except:
         return
-    val = convert(val_in * q.degF, to = q.degC) 
-    out_str = "{}C corresponds to {:.2f}F!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
 
 " Distance "
@@ -64,26 +78,22 @@ m_match = '(.*)m'
 @respond_to(m_match, re.IGNORECASE)
 @listen_to(m_match, re.IGNORECASE)
 def MtoInch(message, incoming_message):
-    print("Responding to", incoming_message)
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("c",""))
+        val_in = parse_value(incoming_message, "m")
+        out_str = generate_ouput(val_in, q.m, q.inch)
     except:
         return
-    val = convert(val_in * q.m, to = q.inch) 
-    out_str = "{} m corresponds to {:.2f} \"!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
 
 cm_match = '(.*)cm'
 @respond_to(cm_match, re.IGNORECASE)
 @listen_to(cm_match, re.IGNORECASE)
-def InchtoM(message, incoming_message):
-    print("Responding to", incoming_message)
+def CMtoInch(message, incoming_message):
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("c",""))
+        val_in = parse_value(incoming_message, "cm")
+        out_str = generate_ouput(val_in, q.cm, q.inch)
     except:
         return
-    val = convert(val_in * q.cm, to = q.inch) 
-    out_str = "{} m corresponds to {:.2f} \"!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
 
 inch_match = '(.*)"'
@@ -93,37 +103,36 @@ inch_match1 = '(.*)in'
 @respond_to(inch_match1, re.IGNORECASE)
 @listen_to(inch_match1, re.IGNORECASE)
 def CMtoInch(message, incoming_message):
-    print("Responding to", incoming_message)
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("c",""))
+        val_in = parse_value(incoming_message, "in")
+        out_str = generate_ouput(val_in, q.inch, q.cm)
+    except:
+        pass
+    try:
+        val_in = parse_value(incoming_message, "\"")
+        out_str = generate_ouput(val_in, q.inch, q.cm)
     except:
         return
-    val = convert(val_in * q.inch, to = q.m) 
-    out_str = "{} \" corresponds to {:.2f} m!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
 
 mile_match = '(.*)mile'
 @respond_to(mile_match, re.IGNORECASE)
 @listen_to(mile_match, re.IGNORECASE)
 def MiletoKM(message, incoming_message):
-    print("Responding to", incoming_message)
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("c",""))
+        val_in = parse_value(incoming_message, "mile")
+        out_str = generate_ouput(val_in, q.mile, q.km)
     except:
         return
-    val = convert(val_in * q.mile, to = q.km) 
-    out_str = "{} mile corresponds to {:.2f} km!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
 
 km_match = '(.*)km'
 @respond_to(km_match, re.IGNORECASE)
 @listen_to(km_match, re.IGNORECASE)
 def KMtoMile(message, incoming_message):
-    print("Responding to", incoming_message)
     try:
-        val_in = float(incoming_message.split()[-1].lower().replace("c",""))
+        val_in = parse_value(incoming_message, "mile")
+        out_str = generate_ouput(val_in, q.km, q.mile)
     except:
         return
-    val = convert(val_in * q.km, to = q.mile) 
-    out_str = "{} km corresponds to {:.2f} mile!".format(val_in, float(val.magnitude))
     dispatch(message, out_str)
